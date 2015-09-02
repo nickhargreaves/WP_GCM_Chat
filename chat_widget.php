9f53e091b96b8e5e25211342b0b844a22b78d6b0
Nick Hargreaves <hargreaves1234@gmail.com>
2015-09-02 22:11:49 +0300

    create dashboard chat widget

--- chat_widget.php
+++ chat_widget.php
@@ -19,5 +19,30 @@ add_action( 'wp_dashboard_setup', 'wp_gcm_chat_add_dashboard_widgets' );
 
 function wp_gcm_chat_dashboard_widget_function() {
 
+    ?>
+    <div id="chatContainer">
+
+        <div id="chatTopBar" class="rounded"></div>
+        <div id="chatLineHolder"></div>
+
+        <div id="chatUsers" class="rounded"></div>
+        <div id="chatBottomBar" class="rounded">
+            <div class="tip"></div>
+
+            <form id="loginForm" method="post" action="">
+                <input id="name" name="name" class="rounded" maxlength="16" />
+                <input id="email" name="email" class="rounded" />
+                <input type="submit" class="blueButton" value="Login" />
+            </form>
+
+            <form id="submitForm" method="post" action="">
+                <input id="chatText" name="chatText" class="rounded" maxlength="255" />
+                <input type="submit" class="blueButton" value="Submit" />
+            </form>
+
+        </div>
+
+    </div>
+    <?php
 
 }
